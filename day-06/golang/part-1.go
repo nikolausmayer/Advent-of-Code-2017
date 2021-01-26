e6bfadb7624c8366ddd7a571847fb35c801c690c
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2021-01-26 20:35:37 +0100

    Day 6 Go

---
+++ day-06/golang/part-1.go
@@ -0,0 +1,56 @@
+package main
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "strconv"
+  "strings"
+)
+
+func main() {
+  if len(os.Args) < 2 {
+    return
+  }
+  infile, err := os.Open(os.Args[1])
+  if err != nil { return }
+  scanner := bufio.NewScanner(infile)
+  scanner.Scan()
+  line := strings.Fields(scanner.Text())
+  var banks []int
+  for _, v := range line {
+    i, err := strconv.Atoi(v)
+    if err != nil { return }
+    banks = append(banks, i)
+  }
+  visited := make(map[int]struct{})
+  for {
+    idx, maxe := 0, 0
+    for i, e := range banks {
+      if e > maxe {
+        idx, maxe = i, e
+      }
+    }
+
+    key := 0
+    for _, e := range banks {
+      key = key * (maxe + 1) + e
+    }
+    _, ok := visited[key]
+    if ok {
+      break
+    }
+    visited[key] = struct{}{}
+
+    toDistribute := banks[idx]
+    banks[idx] = 0
+    idx++
+    for ;toDistribute > 0; {
+      idx = idx % len(banks)
+      banks[idx]++
+      idx++
+      toDistribute--
+    }
+  }
+  fmt.Println(len(visited))
+}
+
