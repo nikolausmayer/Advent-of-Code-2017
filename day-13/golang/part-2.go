01d77f962c344990f39dd11f97a13f4584f2fe3c
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2021-02-02 20:59:39 +0100

    Day 13 Go

--- day-13/golang/part-2.go
+++ day-13/golang/part-2.go
@@ -7,54 +7,6 @@ import (
   "strings"
 )
 
-func isPrime(n int) bool {
-  for i := 2; i*i <= n; i++ {
-    if n % i == 0 {
-      return false
-    }
-  }
-  return true
-}
-
-func nextPrime(n int) int {
-  n++
-  for ; !isPrime(n); n = nextPrime(n) { }
-  return n
-}
-
-func primeFactors(n int) map[int]int {
-  results := make(map[int]int)
-  for i := 2; n > 1; {
-    if n % i == 0 {
-      results[i]++
-      n /= i
-    } else {
-      i++
-    }
-  }
-  return results
-}
-
-func coPeriod(n int, m int) int {
-  pfn := primeFactors(n)
-  pfm := primeFactors(m)
-
-  for pf, pow := range pfm {
-    if pow > pfn[pf] {
-      pfn[pf] = pow
-    }
-  }
-
-  result := 1
-  for pf, pow := range pfn {
-    for i := 0; i < pow; i++ {
-      result *= pf
-    }
-  }
-  return result
-}
-
-
 func main() {
   if len(os.Args) < 2 {
     return
@@ -71,16 +23,6 @@ func main() {
     length, _ := strconv.Atoi(line[1])
     scanners[depth] = length
   }
-
-  //start := 1
-  //period := 1
-
-  //for d, p := range scanners {
-  //  if d % 2*(p-1) == 0 {
-  //    start++
-  //  }
-  //  period = coPeriod(period, 2*(p-1))
-  //}
 
   for i := 0;; i++ {
     stop := true
