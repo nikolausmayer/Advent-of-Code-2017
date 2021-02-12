1c2763d16f814b14c12500a4e3a266130458f836
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2021-02-12 21:02:46 +0100

    Day 23 Go

--- day-23/golang/part-2.go
+++ day-23/golang/part-2.go
@@ -5,7 +5,10 @@ import (
 
 func main() {
   fmt.Println("How very unusual for Advent of Code.")
-  fmt.Println("This program should the number of non-prime numbers between 108100 and 125100 (inclusive) which are representable as 108100+17*i for a positive integer i")
+  fmt.Println()
+  fmt.Println("This program should count the number of non-prime numbers between 108100 and 125100 (inclusive) which are representable as 108100+17*i for a nonnegative integer i")
+  fmt.Println()
+  fmt.Println("If anyone knows how to NOT work this out on paper, please tell me!")
   fmt.Println()
   fmt.Println(909)
 }
